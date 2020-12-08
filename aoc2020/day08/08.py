03c39800abca0eedba6385442286a672a41fa673
Bradley West <bwest@wayfair.com>
2020-12-08 16:26:29 -0500

    day 8 cleanup

--- aoc2020/day08/08.py
+++ aoc2020/day08/08.py
@@ -1,18 +1,16 @@
-
-
-with open('input_08.txt', 'r') as f:
-    lines = f.read().splitlines()
-
-#part 1
 def doPart1(lines, verbose = False):
-    repeat = False
-    acc = 0
-    rule_index = 0
-    rule_history = []
+    repeat = False # boolean flag if a rule has been repeated
+    acc = 0        # global accumulator value
+    rule_index = 0 # line index for current rule
+    rule_history = [] #line history for previous rules
+
     while repeat is False and rule_index != len(lines):
+        #get current rule 
         line = lines[rule_index]
         if verbose: print(f"{rule_index}\t{acc}\t{line}")
         rule_history.append(rule_index)
+        
+        #read the rule and do the action
         op, value = line.split(' ')
         if op == 'acc':
             acc += int(value)
@@ -21,9 +19,12 @@ def doPart1(lines, verbose = False):
             rule_index += 1
         elif op == 'jmp':
             rule_index += int(value)
+        
+        #check if rule is a repeat
         if rule_index in rule_history:
             if verbose: print(f"Repeated Operatation:\n\tAccumulator value = {acc}")
             repeat = True
+        
     return acc, rule_history, repeat
 
 def line_edit(lines, index):
@@ -34,6 +35,11 @@ def line_edit(lines, index):
         lines[index] = 'nop ' + val
     return lines
 
+#read the rules
+with open('input_08.txt', 'r') as f:
+    lines = f.read().splitlines()
+
+#part 1
 acc, rule_history, repeat = doPart1(lines)
 if repeat: print(f"Repeated Operatation:\n\tAccumulator value = {acc}")
 
